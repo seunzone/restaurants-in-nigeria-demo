1eb7f8272f00819de30893fa4c079d82a209bbbd
seunzone <darealseun@gmail.com>
2017-08-22 12:00:56 +0100

    Testinf saved data from database

--- app.js
+++ app.js
@@ -21,18 +21,18 @@ var foodSchema = new mongoose.Schema({
 
 var Restaurant = mongoose.model("Restaurant", foodSchema);
 
-Restaurant.create(
+// Restaurant.create(
 
 
-    {name: "Mama Lokoja", image:"https://scontent.flos3-1.fna.fbcdn.net/v/t1.0-1/p160x160/20767815_1577821268935675_1650698914966330585_n.jpg?_nc_eui2=v1%3AAeFu6tl94dolWmOLddc6bBX126ntjmrFBRbMOSPLhqaJkNJBzr-GZotQhnAazP5DSbdqRcsHm5ZFO24epDfeBByUmYftqfsBHjGkanAfOQ47Gw&oh=20c5bb7fe50a75b4bc2f88cc9e6781e8&oe=5A37CF0D"}, function(err, restaurant){
-        if(err){
-            console.log(err);
-        } else{
-            console.log("New Food Site: ");
-            console.log(restaurant);
-        }
-    }
-);
+//     {name: "Mama Lokoja", image:"https://scontent.flos3-1.fna.fbcdn.net/v/t1.0-1/p160x160/20767815_1577821268935675_1650698914966330585_n.jpg?_nc_eui2=v1%3AAeFu6tl94dolWmOLddc6bBX126ntjmrFBRbMOSPLhqaJkNJBzr-GZotQhnAazP5DSbdqRcsHm5ZFO24epDfeBByUmYftqfsBHjGkanAfOQ47Gw&oh=20c5bb7fe50a75b4bc2f88cc9e6781e8&oe=5A37CF0D"}, function(err, restaurant){
+//         if(err){
+//             console.log(err);
+//         } else{
+//             console.log("New Food Site: ");
+//             console.log(restaurant);
+//         }
+//     }
+// );
 
 
 
@@ -68,9 +68,17 @@ app.post("/restaurants", function(req, res){
 var name = req.body.name;
 var image = req.body.image;
 var newRestaurant = {name: name, image:image}
-restaurants.push(newRestaurant);
+//New Restaurant fro Database
+Restaurant.create(newRestaurant, function(err, newlyCreated){
+    if(err){
+        console.log(err);
+    } else {
+        res.redirect("/restaurants");
+    }
 
-res.redirect("/restaurants");
+});
+
+
 });
 
 app.get("/restaurants/new", function(req, res){
